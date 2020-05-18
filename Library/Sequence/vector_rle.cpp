8cf9c1f6c24bd5d32a33cb74eb059b40a8c88c45
Yung-Luen Lan <yllan@me.com>
2020-05-18 18:04:49 +0800

    Cleanup the vector_rle

--- Library/Sequence/vector_rle.cpp
+++ Library/Sequence/vector_rle.cpp
@@ -1,35 +1,31 @@
+#include<vector>
+#include<tuple>
+#include<iostream>
+using namespace std;
+
 /* [2, 2, 2, 2, 3, 11, 11, 11] -> [(2, 4), (5, 1), (11, 3)] */
 template<typename T>
 vector<pair<T, int>> rle_compress(vector<T> list) {
-  bool first = true;
-  T last;
-  int count = 0;
-  vector<pair<T, int>> result;
-  
+  vector<pair<T, int>> r;
   for (auto e : list) {
-    if (first) {
-      last = e;
-      count = 1;
-      first = false;
-    } else if (e == last) {
-      count++;
-    } else {
-      result.push_back(make_pair(last, count));
-      last = e;
-      count = 1;
-    }
+    if (r.empty() || e != r.back().first) r.emplace_back(e , 1);
+    else (r.end() - 1)->second++;
   }
-  if (!first) result.push_back(make_pair(last, count));
-  return result;
+  return r;
 }
 
 /* [(A, 3), (B, 4)] -> [A, A, A, B, B, B, B] */
-template<typename T> 
+template<typename T>
 vector<T> rle_expand(vector<pair<T, int>> z) {
-  vector<T> result;
-  for (auto p : z) {
-    for (int i = 0; i < p.second; i++) 
-      result.push_back(p.first);
+  vector<T> r;
+  for (auto p : z) for (int i = 0; i < p.second; i++) r.push_back(p.first);
+  return r;
+}
+
+int main() {
+  vector<int> xs = {1,1,1,1,2,3,3,4};
+  vector<pair<int, int>> ys = rle_compress(xs);
+  for (auto y : ys) {
+    cout << y.first << ":" << y.second << endl;
   }
-  return result;
 }
